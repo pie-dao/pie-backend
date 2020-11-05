245197a6fc395b22d85e65aaf2e73d7835dc10a5
alexintosh <alessio.d@gmail.com>
2020-11-05 15:59:17 +0100

    Populate Coin Entity

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,20 @@
-import { createConnection } from 'typeorm';
+import { createConnection, Repository, Connection } from 'typeorm';
+import {Coin} from './entities/coin';
+
+let connection: Connection;
+let coinsRepo: Repository<Coin>;
 
 export const connectDB = async () => {
-  await createConnection();
+    connection = await createConnection();
+    coinsRepo = connection.getRepository(Coin);
 };
+
+const getRepos = () => {
+    return {
+        coinsRepo
+    }
+}
+
+export const db = {
+    getRepos
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import 'dotenv/config';
 import * as express from 'express';
-import { connectDB } from './database'
+import { connectDB, db } from './database'
+import { Coin } from './entities/coin';
+
+const CoinGecko = require('coingecko-api');
 
 const app = express();
 
@@ -10,6 +13,26 @@ const app = express();
 app.get('/api', (req, res, next) => {
   res.status(200).json({
     hello: 'World!',
+  });
+});
+
+app.get('/populateCoin/:coingeckoId', async (req, res) => {
+  const CoinGeckoClient = new CoinGecko();
+  let resApi: any = await CoinGeckoClient.coins.fetch(req.params.coingeckoId, {});
+  const { coinsRepo } = db.getRepos();
+  
+  let coin = new Coin();
+  coin.symbol = resApi.data.symbol;
+  coin.name = resApi.data.name;
+  coin.coingeckoId = req.params.coingeckoId;
+  await coinsRepo.save(coin);
+  console.log("Coin has been saved");
+
+  const all = await coinsRepo.find();
+
+  res.status(200).json({
+    id: req.params.coingeckoId,
+    all 
   });
 });
 
