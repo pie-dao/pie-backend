193b531de6406873f214d9af3c164ec9c20a5540
alexintosh <alessio.d@gmail.com>
2020-11-12 21:59:52 +0100

    new tslit

--- ormconfig.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    "type": "postgres",
-    "url": process.env.DATABASE_URL,
-    "host": "localhost",
-    "port": 5432,
-    "synchronize": true,
-    "logging": true,
-    "entities": ["src/entities/**/*.ts", "dist/entities/**/*.js"]
-}
